fae038d30abc407b812fba4c3dae44f705679ce0
Frank Matranga <thefrankmatranga@gmail.com>
2021-01-24 12:42:15 -0500

    Dynamic command system

---
+++ commands/courses.js
@@ -0,0 +1,21 @@
+const { Course, CourseEnrollment, CourseTeam } = require("./db");
+
+module.exports = {
+    name: 'courses',
+    description: 'Manage courses with their categories, channels, and roles.',
+    serverOnly: true,
+    usages: {
+        'courses add <course name> <course short name> <number of teams>': 'Create a course and a category...',
+        'courses reset <course name or id>': 'Wipe the course channels\'s messages and remove access from students.',
+        'courses remove <course name or id>': 'Delete the course category, role, and all channels.'
+
+    },
+    examples: [
+        'courses add "Intro to ITWS" "intro" 10',
+        'courses reset "Intro to ITWS"',
+        'courses remove "Intro to ITWS"'
+    ], 
+    execute(message, args) {
+        message.channel.send('Test');
+    }
+};
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const dotenv = require("dotenv")
 dotenv.config()
 
+const fs = require("fs");
+
 // Import Discord Node Module
 const Discord = require("discord.js");
 
@@ -10,22 +12,20 @@ const commandPrefix = process.env.DISCORD_COMMAND_PREFIX;
 
 const itwsServerId = process.env.DISCORD_SERVER_ID;
 
-require("./db");
-
 // Define Roles with id's
 /** Role names matched to role IDs */
 const roles = {};
 
-/** Message to send for the help command */
-const helpMessage = [
-    "**List of commands:**",
-    "-----------------",
-    `\`${commandPrefix}help\` - bring up this prompt`,
-    `\`${commandPrefix}role roleName\` - add yourself to a role (use \`${commandPrefix}role list\` to recive a direct message with a list of all of the roles`
-].join("\n")
-
 /** Bot object */
 const bot = new Discord.Client();
+
+// Load commands from commands folder
+bot.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    bot.commands.set(command.name, command);
+}
 
 function getRoleByName(roleName) {
     return bot.guilds.cache.get(itwsServerId).roles.cache.find(role => role.name === roleName)
@@ -34,98 +34,103 @@ function getRoleByName(roleName) {
 bot.once("ready", async () => {
     console.log(`Bot is ready with command prefix ${commandPrefix}`);
 
-    // Setup roles by name
-    roles['Intro'] = getRoleByName('Intro to ITWS').id;
+    // // Setup roles by name
+    // roles['Intro'] = getRoleByName('Intro to ITWS').id;
 
-    for (let gradYear = 21; gradYear <= 24; gradYear++) {
-        roles['Class' + gradYear] = getRoleByName('Class of 20' + gradYear).id;
-    }
+    // for (let gradYear = 21; gradYear <= 24; gradYear++) {
+    //     roles['Class' + gradYear] = getRoleByName('Class of 20' + gradYear).id;
+    // }
 
-    roles['Graduate'] = getRoleByName('Graduate').id;
+    // roles['Graduate'] = getRoleByName('Graduate').id;
 
-    for (let team = 1; team <= 19; team++) {
-        roles['Team' + team] = getRoleByName('Intro Team ' + team).id;
-    }
+    // for (let team = 1; team <= 19; team++) {
+    //     roles['Team' + team] = getRoleByName('Intro Team ' + team).id;
+    // }
 
-    roles['Capstone'] = getRoleByName('Capstone').id;
-    for (let team = 1; team <= 6; team++) {
-	    roles['Capstone' + team] = getRoleByName('Capstone Team ' + team).id;
-    }
+    // roles['Capstone'] = getRoleByName('Capstone').id;
+    // for (let team = 1; team <= 6; team++) {
+    //     roles['Capstone' + team] = getRoleByName('Capstone Team ' + team).id;
+    // }
 
-    roles['AmongUs'] = getRoleByName('Among Us').id;
-    roles['DnD'] = getRoleByName('DnD').id;
-    roles['test_role'] = getRoleByName('test_role').id;
-    // console.log(roles);
+    // roles['AmongUs'] = getRoleByName('Among Us').id;
+    // roles['DnD'] = getRoleByName('DnD').id;
+    // roles['test_role'] = getRoleByName('test_role').id;
+    // // console.log(roles);
 });
 
 bot.on("message", (message) => {
-    // Ignore non-commands
-    if (!message.content.startsWith(commandPrefix)) return;
+    // Ignore non-commands and bot messages
+    if (!message.content.startsWith(commandPrefix) || message.author.bot) return;
 
     // Divide input into parts
     // Command will be the first part and args will be the arguments
     // e.g. "!role ITWS" -> command="!role", args=["ITWS"]
-    let [command, ...args] = message.content.split(" ");
+    const args = message.content.slice(commandPrefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
 
-    // Remove command prefix
-    command = command.replace(commandPrefix, "");
+    if (!bot.commands.has(commandName)) return;
 
-    // Help command
-    if (command === "help") {
-        message.channel.send(helpMessage);
-    }
-
-    // Role command
-    else if (command === "role") {
-        const desiredRoleName = args[0]
-
-        if (desiredRoleName == "list") {
-            // List all roles
-            message.member.send("**Roles**\n" + Object.keys(roles).join("\n"));
-        } else if (desiredRoleName in roles) {
-            // User chose a valid role
-            message.member.roles.add(roles[desiredRoleName])
-	    message.member.send("Added " + desiredRoleName);
-        } else {
-            // User chose an invalid role
-            message.channel.send("That's not a valid role!");
+    // Attempt to run command
+    try {
+        const command = bot.commands.get(commandName);
+        if (command.serverOnly && message.channel.type === 'dm') {
+            return message.reply('Must use the command in a server!');
         }
-    } 
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('Oops! Something went wrong running that command... Please let a Moderator know.');
+    }
     
-    else if (command === "remove") {
-        let desiredRoleName = args[0];
+    // // Role command
+    // else if (command === "role") {
+    //     const desiredRoleName = args[0]
 
-        // !remove range <role> <start> <end>
-        if(desiredRoleName === "range") {    
-            desiredRoleName = args[1]
-            for(var team = args[2]; team <= args[3]; team++) {
-                message.guild.members.fetch().then((member) => member.map((user, v) => {
-                    user.roles.cache.forEach((r) => {
-                        if(r.id == roles[desiredRoleName.toString()+team.toString()]) {
-                            user.roles.remove(roles[desiredRoleName.toString()+team.toString()]);
-                        }
-                    })
-                }));
-            }
-        }
-        
+    //     if (desiredRoleName == "list") {
+    //         // List all roles
+    //         message.member.send("**Roles**\n" + Object.keys(roles).join("\n"));
+    //     } else if (desiredRoleName in roles) {
+    //         // User chose a valid role
+    //         message.member.roles.add(roles[desiredRoleName])
+    //         message.member.send("Added " + desiredRoleName);
+    //     } else {
+    //         // User chose an invalid role
+    //         message.channel.send("That's not a valid role!");
+    //     }
+    // }
 
-        else {
-            if (desiredRoleName in roles) {
-                message.guild.members.fetch().then((member) => member.map((user, v) => {
-                    user.roles.cache.forEach((r) => {
-                        if(r.id == roles[desiredRoleName]) {
-                            user.roles.remove(roles[desiredRoleName]);
-                        }
-                    })
-                }));
-            } 
-            
-            else {
-                message.channel.send("That's not a valid role!");
-            }
-        }
-    }
+    // else if (command === "remove") {
+    //     let desiredRoleName = args[0];
+
+    //     // !remove range <role> <start> <end>
+    //     if (desiredRoleName === "range") {
+    //         desiredRoleName = args[1]
+    //         for (var team = args[2]; team <= args[3]; team++) {
+    //             message.guild.members.fetch().then((member) => member.map((user, v) => {
+    //                 user.roles.cache.forEach((r) => {
+    //                     if (r.id == roles[desiredRoleName.toString() + team.toString()]) {
+    //                         user.roles.remove(roles[desiredRoleName.toString() + team.toString()]);
+    //                     }
+    //                 })
+    //             }));
+    //         }
+    //     }
+    //     else {
+    //         if (desiredRoleName in roles) {
+    //             message.guild.members.fetch().then((member) => member.map((user, v) => {
+    //                 user.roles.cache.forEach((r) => {
+    //                     if (r.id == roles[desiredRoleName]) {
+    //                         user.roles.remove(roles[desiredRoleName]);
+    //                     }
+    //                 })
+    //             }));
+    //         }
+
+    //         else {
+    //             message.channel.send("That's not a valid role!");
+    //         }
+    //     }
+    // }
 
 
     /*else if (command == "teamrolegenerate") {
