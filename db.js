bdde6651470a305b78407ce72e8dad96c6f9e2ad
Frank Matranga <thefrankmatranga@gmail.com>
2021-01-24 19:06:31 -0500

    Disable SQL logging

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    logging: false
+});
 
 const Course = sequelize.define("Course", {
     title: {
