16f3715c0d40dae15d074238a60608e6d1ee2e2b
Frank Matranga <thefrankmatranga@gmail.com>
2021-02-19 16:09:39 -0500

    Document parseCommandAndArgs

--- utils.js
+++ utils.js
@@ -1,3 +1,18 @@
+/**
+ * Splits a message into a command name and its arguments.
+ * Assumes that the command prefix has already been removed.
+ * The command name is lowercased. It treats arguments in
+ * quotation marks as single arguments.
+ * 
+ * ### Examples
+ * - ECHO hello world -> ["echo", "hello", "world"]
+ * - echo "hello world" -> ["echo", "hello world"]
+ * - eCHo goodbye "hello world" -> ["echo", "goodbye", "hello world"]
+ * 
+ * ### Returns
+ * An array with the comamnd name followed by optional arguments.
+ * Can be used in destructuring like `const [command, args] = parseCommandAndArgs(line);`
+ */
 function parseCommandAndArgs(line) {
     line = line.trim();
     const regex = new RegExp('"[^"]+"|[\\S]+', 'g');
