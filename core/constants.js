b04a281c08d16ef372d4f441c401f5e752277cd9
Frank Matranga <thefrankmatranga@gmail.com>
2022-05-27 19:34:06 -0400

    Add constants

---
+++ core/constants.js
@@ -0,0 +1,20 @@
+/**
+ * Used in displaying the possible roles to users, as well as determining what roles to assign/remove to users.
+ */
+module.exports.userRoles = [
+  {
+    label: 'Prospective Students',
+    customId: 'prospective',
+    discordRoleId: '979889951594381382',
+  },
+  {
+    label: 'Accepted Students',
+    customId: 'accepted',
+    discordRoleId: '979874983650148382',
+  },
+  {
+    label: 'Alumni',
+    customId: 'alumni',
+    discordRoleId: '979874881481089084',
+  },
+];
