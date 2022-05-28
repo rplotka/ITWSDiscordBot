3aac84655d7c30651b80180242269f665a22c7e6
Frank Matranga <thefrankmatranga@gmail.com>
2022-05-28 13:31:47 -0400

    Remove old utils

--- core/utils.js
+++ core/utils.js
@@ -296,44 +296,6 @@ async function removeCourse(guild, course) {
 }
 
 /**
- * Splits a message into a command name and its arguments.
- * Assumes that the command prefix has already been removed.
- * The command name is lowercased. It treats arguments in
- * quotation marks as single arguments.
- *
- * @param {string} lineRaw asdasdasdasd
- * @return {[string, string[]]} [command name, arg array]
- *
- * ### Examples
- * - ECHO hello world -> ["echo", ["hello", "world"]]
- * - echo "hello world" -> ["echo", ["hello world"]]
- * - eCHo goodbye "hello world" -> ["echo", ["goodbye", "hello world"]]
- */
-function parseCommandAndArgs(lineRaw) {
-  const line = lineRaw.trim();
-  const regex = new RegExp('"[^"]+"|[\\S]+', 'g');
-  const args = [];
-  line.match(regex).forEach((element) => {
-    if (!element) return;
-    args.push(element.replace(/"/g, ''));
-  });
-  return [args[0].toLowerCase(), args.slice(1)];
-}
-
-/**
- * Attempts to fetch a member from a server
- * from their Discord user ID. Will only work if the
- * user is on the server.
- *
- * @param {Discord.Guild} server
- * @param {string} userId
- * @return {Promise<Discord.GuildMember>} The found member (if exists)
- */
-function fetchMemberById(server, userId) {
-  return server.members.fetch(userId);
-}
-
-/**
  * Attempts to toggle a specific role on a server member.
  *
  * @param {Discord.GuildMember} member Discord server member
@@ -361,7 +323,5 @@ module.exports = {
   addMemberToCourseTeam,
   removeMemberFromCourseTeam,
   findCourseGeneralChannel,
-  parseCommandAndArgs,
-  fetchMemberById,
   toggleMemberRole,
 };
